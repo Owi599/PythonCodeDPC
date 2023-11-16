e4619a32771dc9edae6a5b6df8cd1b2bf0a71ab2
s_oalsabba <oalsabba@st.ovgu.de>
2023-11-16 17:29:04 +0100

    something

---
+++ matrix.py
@@ -0,0 +1,15 @@
+import numpy as np
+import sympy as sp
+from sympy import symbols as sym
+from sympy import Matrix as mat
+
+# Constants
+alpha1 = sym('alpha1')
+alpha2 = sym('alpha2')
+
+# define constant symbols
+A = mat([[1, 0, 0],[0, 1, alpha1],[0, alpha2, 1]])
+
+inversed = A.inv()
+
+print(inversed)
