bf27bc338d07ab3342fa738f0e1aa40c85044c1a
Owi599 <alsabbaghowis@gmail.com>
2024-04-15 16:26:00 +0200

    update

--- LQR.py
+++ LQR.py
@@ -2,54 +2,16 @@ import numpy as np
 import control as ct
 import matplotlib.pyplot as plt
 from scipy.linalg import solve_continuous_are
-import socket
+import time
+from udp import UDP
 
 # define UDP server
-UDP_IP = "192.168.2.106"
+UDP_IP = "10.0.3.40"
+UDP_PI = "10.0.3.55"
 UDP_PORT_RECV_ALPHA = 890
 UDP_PORT_RECV_BETA = 892
 UDP_PORT_RECV_X = 893
-UDP_PORT_SEND = 891
-sock_recv_a = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock_recv_b = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock_recv_x = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-sock_send = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-
-def UDP_Angle_Alpha():
-    while True:
-        sock_recv_a.bind((UDP_IP, UDP_PORT_RECV_ALPHA))
-        data, addr = sock_recv_a.recvfrom(1024)  # buffer size is 1024 bytes
-        return data.decode()
-
-
-def UDP_Angle_Beta():
-    while True:
-        sock_recv_b.bind((UDP_IP, UDP_PORT_RECV_BETA))
-        data, addr = sock_recv_b.recvfrom(1024)  # buffer size is 1024 bytes
-        return data.decode()
-
-
-def UDP_Cart_Position():
-    while True:
-        sock_recv_x.bind((UDP_IP, UDP_PORT_RECV_X))
-        data, addr = sock_recv_x.recvfrom(1024)  # buffer size is 1024 bytes
-        return data.decode()
-
-
-def UDP_send_data(data):
-    sock_send.sendto(data.encode(), (UDP_IP, UDP_PORT_SEND))
-
-
-A = UDP_Angle_Alpha()
-B = UDP_Angle_Beta()
-C = UDP_Cart_Position()
-
-# convert strings to float values and split
-A = float(A)
-B = float(B)
-C = float(C)
-
+UDP_PORT_SEND = 5000
 
 # Parameter defintion
 pi = np.pi
@@ -67,8 +29,6 @@ Bc = 0.5
 B1 = 0.001
 B2 = 0.001
 
-x0 = np.array([[C], [A], [B], [0.0], [0.0], [0.0]])  # initial state Vector
-
 # Intermediates
 h1 = mc + m1 + m2
 h2 = m1 * LC1 + m2 * L1
@@ -78,6 +38,19 @@ h5 = m2 * LC2 * L1
 h6 = m2 * LC2**2 + I2
 h7 = m1 * LC1 * g + m2 * L1 * g
 h8 = m2 * LC2 * g
+
+udpA = UDP(UDP_IP, UDP_PORT_RECV_ALPHA)
+udpB = UDP(UDP_IP, UDP_PORT_RECV_BETA)
+udpX = UDP(UDP_IP, UDP_PORT_RECV_X)
+
+
+Alpha = udpA.Rec_Message()[0]
+Beta = udpB.Rec_Message()[0]
+x = udpX.Rec_Message()[0]
+
+udpSend = UDP(UDP_PI,UDP_PORT_SEND)
+
+x0 = np.array([[Alpha], [Beta], [x], [0], [0], [0]])
 
 # Dynamics
 M = np.array(
@@ -106,8 +79,6 @@ A = np.linalg.solve(M, N)
 B = np.linalg.solve(M, F)
 C = np.array([1, 0, 0, 0, 0, 0])
 D = 0
-Ctrl = ct.ctrb(A, B)
-Rank = np.linalg.matrix_rank(Ctrl)
 
 Q = np.array(
     [
@@ -119,43 +90,19 @@ Q = np.array(
         [0, 0, 0, 0, 0, 1],
     ]
 )
-R = np.array([[10]])
+R = np.array([[100]])
 
 P = solve_continuous_are(A, B, Q, R)
 K = np.linalg.inv(R) @ B.T @ P
 
 
 def lqr_contol(x):
-    while True:
-        return np.clip(-K @ x, -5, 5)
+    return np.clip(-K @ x, -5, 5)
 
 
 u = lqr_contol(x0)
-print(u)
 
-# K, S, E = ct.lqr(A, B, Q, R)
-# BK = B @ K
-# sys = ct.ss((A - BK), B, C, D)
+print(u[0][0])
 
-# t = np.arange(0, 30, 0.005)
-# t, y, x = ct.initial_response(sys, T=t, X0=x0, return_x=True)
+udpSend.Send_Message(float(u[0][0]))
 
-# # Plot results
-# plt.figure()
-# plt.xlabel("t in s")
-# plt.ylabel("x in m / theta in rad")
-# plt.plot(t, x[1], "b--", label="theta_1")
-# plt.plot(t, x[2], "r:", label="theta_2")
-# plt.plot(t, x[0], "g", label="x")
-# plt.legend(loc="best")
-# plt.grid(True)
-
-# plt.figure()
-# plt.xlabel("t in s")
-# plt.ylabel("x_dot in m/s / theta_dot in rad/s")
-# plt.plot(t, x[4], "b--", label="theta_1_dot")
-# plt.plot(t, x[5], "r:", label="theta_2_dot")
-# plt.plot(t, x[3], "g", label="x_dot")
-# plt.legend(loc="best")
-# plt.grid(True)
-# plt.show()
