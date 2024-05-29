5170026dbb9f3e09f023685aca05d5eb8375f2a8
Owi599 <alsabbaghowis@gmail.com>
2024-05-29 10:52:05 +0200

    single pendulum

--- LQR_singular_pendulum.py
+++ LQR_singular_pendulum.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 from scipy.linalg import solve_continuous_are
 import time
 from com import COM
+import random
 
 # define UDP server
 ETH_IP = "10.0.8.40"
@@ -20,27 +21,18 @@ UDP = 1
 pi = np.pi
 mc = 0.232
 m1 = 0.127
-m2 = 0.127
 L1 = 0.3
-L2 = 0.3
 LC1 = 0.3
-LC2 = 0.3
 I1 = m1 * LC1**2
-I2 = m2 * LC2**2
 g = 9.81
 Bc = 0.5
 B1 = 0.001
-B2 = 0.001
 
 # Intermediates
-h1 = mc + m1 + m2
-h2 = m1 * LC1 + m2 * L1
-h3 = m2 * LC2
-h4 = m2 * LC1**2 + m2 * L1**2 + I1
-h5 = m2 * LC2 * L1
-h6 = m2 * LC2**2 + I2
-h7 = m1 * LC1 * g + m2 * L1 * g
-h8 = m2 * LC2 * g
+h1 = mc + m1 
+h2 = m1 * LC1 
+h3 =  m1 * L1**2 + I1
+h4 = m1 * LC1 * g
 
 udpA = COM(ETH_IP, UDP_PORT_RECV_ALPHA,UDP)
 # udpB = COM(ETH_IP, UDP_PORT_RECV_BETA,UDP)
@@ -58,58 +50,59 @@ udpSend = COM(ETH_IP_PI,UDP_PORT_SEND,UDP)
 # Dynamics
 M = np.array(
     [
-        [1, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, h1, h2, h3],
-        [0, 0, 0, h2, h4, h5],
-        [0, 0, 0, h3, h5, h6],
+        [1, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, h1, h2],
+        [0, 0, h2, h3],
     ]
 )
 N = np.array(
     [
-        [0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1],
-        [0, 0, 0, 0, 0, 0],
-        [0, -h7, 0, 0, 0, 0],
-        [0, 0, -h8, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 0],
+        [0, -h4,0, 0],
     ]
 )
-F = np.array([[0], [0], [0], [1], [0], [0]])
+F = np.array([ [0], [0], [1], [0]])
 
 A = np.linalg.solve(M, N)
 B = np.linalg.solve(M, F)
-C = np.array([1, 0, 0, 0, 0, 0])
+C = np.array([1, 0, 0, 0])
 D = 0
 
 Q = np.array(
     [
-        [3000, 0, 0, 0, 0, 0],
-        [0, 1, 0, 0, 0, 0],
-        [0, 0, 1, 0, 0, 0],
-        [0, 0, 0, 1, 0, 0],
-        [0, 0, 0, 0, 1, 0],
-        [0, 0, 0, 0, 0, 1],
+        [1000, 0, 0, 0],
+        [0, 1, 0, 0],
+        [0, 0, 1, 0],
+        [0, 0, 0, 1],
     ]
 )
-R = np.array([[10]])
+R = np.array([[20]])
 
 P = solve_continuous_are(A, B, Q, R)
 K = np.linalg.inv(R) @ B.T @ P
 
 
 def lqr_contol(x):
-    return np.clip(-K @ x, -20, 20)
+    return np.clip(-K @ x, - 50,50)
 
 while True:
     try:
         Alpha = float(udpA.Rec_Message())
-        x0 = np.array([[0], [Alpha], [0], [0], [0], [0]])  
+        X = random.uniform(-0.5,0.5)
+        # if Alpha < 0:
+        #         Alpha = Alpha + 2*np.pi
+        
+        x0 = np.array([[X], [Alpha], [0], [0]])  
         time.sleep(0.001) 
         u = lqr_contol(x0)
-        print(u[0][0])
+        print(u[0][0],'',)
         udpSend.Send_Message(float(u[0][0]))
+        
     except KeyboardInterrupt:
         break
+    except Exception as e:
+        print("An error occurred:", str(e))
     
